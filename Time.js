290c8ddf296b1b8d9140a1771b54165ce38b1e80
jonashw <2jonwilson@gmail.com>
2012-10-21 23:45:10 -0700

    Added events 'stop','start', 'refreshrate_changed' and 'all dead' to Time.  All button states are now determined by listeners of these events.  Very clean.

--- Time.js
+++ Time.js
@@ -7,6 +7,7 @@ Time.prototype.start = function(){
 	if(this.isRunning()) return false;
 	var self = this;
 	this.loopInterval = setInterval(function(){self._stepForward()}, 1000/self.ticksPerSecond);
+	EventRegistry.notifyListeners(this,'start');
 }
 Time.prototype.isRunning = function(){
 	return !!this.loopInterval;
@@ -14,6 +15,7 @@ Time.prototype.isRunning = function(){
 Time.prototype.stop = function(){
 	if(this.loopInterval) clearInterval(this.loopInterval);
 	this.loopInterval = null;
+	EventRegistry.notifyListeners(this,'stop');
 }
 Time.prototype.step = function(numTicks){
 	var numTicks = numTicks ? numTicks : 1;
@@ -28,4 +30,5 @@ Time.prototype.setRefreshRate = function(rr){
 	this.ticksPerSecond = rr;
 	this.stop();
 	this.start();
+	EventRegistry.notifyListeners(this,'refreshrate_changed');
 }
